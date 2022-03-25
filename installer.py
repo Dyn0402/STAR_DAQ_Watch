f6b2e2205fa69369cade9a0c09fc91d4ae8956b4
Dylan <Dyn0402@hotmail.com>
2022-03-25 00:42:20 -0700

    Added installer.py to do pyinstaller installation with all added files/options

---
+++ installer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on March 24 10:56 PM 2022
+Created in PyCharm
+Created as STAR_DAQ_Watch/installer.py
+
+@author: Dylan Neff, Dylan
+"""
+
+import PyInstaller.__main__
+
+
+PyInstaller.__main__.run([
+    'main.py',
+    '-y',
+    '-n daq_watch_v4',
+    '--add-data=Alarm04.wav;.',
+    '--add-data=chimes.wav;.',
+    '--add-data=chord.wav;.',
+    '--add-data=notify.wav;.',
+    '--add-data=chromedriver_win.exe;.',
+    '--add-data=geckodriver_win.exe;.',
+])
+
+
+def main():
+    print('donzo')
+
+
+if __name__ == '__main__':
+    main()
